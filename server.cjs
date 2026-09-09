93fb200f9ee08663af9e7764890ca129e26c7415
SaiHarshitha-bit <saiharshi2626@gmail.com>
2026-09-09 22:15:00 +0530

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/../public'));
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your_secret_key_here';
 
